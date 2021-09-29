6db7d09de6d6f033f851e533b2f44fe0cc4b8ca7
Jun Yao <jun.yao@live.com>
2021-09-29 16:38:57 +0800

    prepare dev, train and test datasets
    including text, wav.scp, segments, reco2file_and_channel, utt2spk and spk2utt.

--- egs/tedlium/s5_r3/local/prepare_data.sh
+++ egs/tedlium/s5_r3/local/prepare_data.sh
@@ -7,11 +7,53 @@
 #
 # Apache 2.0
 
+# For more information about data preparation, visit http://kaldi-asr.org/doc/data_prep.html
+# Here are some important files' description.
+
+# The output of the data preparation stage consists of two sets of things. 
+# One relates to "the data" (directories like data/train/) and one relates to "the language" (directories like data/lang/). 
+# The "data" part relates to the specific recordings you have, 
+# and the "lang" part contains things that relate more to the language itself, such as the lexicon, the phone set, 
+# and various extra information about the phone set that Kaldi needs. 
+
+# The first file you need to prepare is "text" which contains the transcriptions of each utterance.
+# The first element on each line is the utterance-id, if you have speaker information in your setup, you should make the speaker-id a prefix of the utterance id
+# The rest of the line is the transcription of each sentence. Format:
+# <utterance-id> <sentence>
+# e.g. sw02001-A_000098-001156 HI UM YEAH I'D LIKE TO TALK ABOUT
+
+# The format of "wav.scp" file is
+# <recording-id> <extended-filename>
+# where the "extended-filename" may be an actual filename, or a command that extracts a wav-format file.
+
+# The format of the "segments" file is:
+# <utterance-id> <recording-id> <segment-begin> <segment-end>
+# where the segment-begin and segment-end are measured in seconds. 
+# These specify time offsets into a recording. The "recording-id" is the same identifier as is used in the "wav.scp" file
+
+# The file "reco2file_and_channel" is only used when scoring (measuring error rates) with NIST's "sclite" tooly. The format is::
+# <recording-id> <filename> <recording-side (A or B)>
+# e.g. sw02001-B sw02001 B
+# The recording side is a concept that relates to telephone conversations where there are two channels, and if not, it's probably safe to use "A".
+
+# The last file you need to create yourself is the "utt2spk" file. The format is
+# <utterance-id> <speaker-id>
+# e.g. sw02001-A_000098-001156 2001-A
+
+# The other files in this directory can be generated from the files you provide.
+# the format of the spk2utt file is <speaker-id> <utterance-id1> <utterance-id2> ....
+# You can create the "spk2utt" file by a command like:
+# utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+
+# sclite is a tool to score speech recognition system output, check https://github.com/usnistgov/SCTK/blob/master/doc/sclite.htm
+
+# in this data, recording-id = filename = speaker-id
+
 # To be run from one directory above this script.
 
 . ./path.sh
 
-# Prepare: test, train,
+# Prepare: dev, test, train,
 for set in dev test train; do
   dir=data/$set.orig
   mkdir -p $dir
@@ -32,6 +74,7 @@ for set in dev test train; do
 ;; LABEL "female" "Female" "Female Talkers"
 ;;'
     # Process the STMs
+    # list lines of stm files and sort them by joint numerical keys: (1，2，4) columns
     cat db/TEDLIUM_release-3/legacy/$set/stm/*.stm | sort -k1,1 -k2,2 -k4,4n | \
       sed -e 's:([^ ]*)$::' | \
       awk '{ $2 = "A"; print $0; }'
